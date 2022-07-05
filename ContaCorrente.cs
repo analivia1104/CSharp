14eae4f61f23e9663bf6472fd66829f71fe7a4ee
analivia1104 <analivia1104@gmail.com>
2022-07-05 16:50:22 -0300

    Envio do código alterando o saldo

--- ContaCorrente.cs
+++ ContaCorrente.cs
@@ -1,21 +1,31 @@
-public class ContaCorrente
+/**********************************************************************************************************************\
+    - metodos são ações
+    - atributos são características
+\**********************************************************************************************************************/
+
+//classe
+public class ContaCorrente 
 {
-    public string Titular;
+    // tipo do modificador de acesso [publico ou privado], tipo [nº inteiro, nº quebrado, letra], nome [nome do objeto] 
+    public string Titular; 
     public int Agencia;
     public int Numero;
     public double Saldo;
 
-    public ContaCorrente (string Titular, int Agencia, int Numero, double Saldo){
+    // construtor: o mesmo nome da classe
+    public ContaCorrente (string Titular, int Agencia, int Numero, double Saldo) { 
+        
+        //this é a representação de um objeto: não é de alguém especifico.
         this.Titular = Titular;
         this.Agencia = Agencia;
         this.Numero = Numero;
         this.Saldo = Saldo; 
-        //this é a representação de um objeto: não é de alguém especifico.
+
     } 
     public bool Sacar( double valorSaque )
     { 
         if(this.Saldo >= valorSaque){
-            this.Saldo = Saldo - valorSaque ;
+            this.Saldo = this.Saldo - valorSaque ;
             return true;
         }
             return false;
--- Program.cs
+++ Program.cs
@@ -7,8 +7,22 @@ namespace FirstProgram //nome do arquivo ".csproj"
         static void Main(string[] args)
         {
             ContaCorrente conta_da_Barbara = new ContaCorrente ("Barbara", 12345, 789, 10 ) ;
-            ContaCorrente conta_da_ana = new ContaCorrente ("Ana",12345, 215, 121540 ) ;
+            ContaCorrente conta_da_ana = new ContaCorrente ("Ana",12345, 215, 1200 ) ;
             ContaCorrente conta_da_jessica = new ContaCorrente ("Jessica", 12345, 858, -598454) ;
+
+            Console.WriteLine ("A conta é do (a) " + conta_da_Barbara.Titular + 
+            ", a agência é " + conta_da_Barbara.Agencia + ", o nº é " + conta_da_Barbara.Numero + "e o saldo é de: "
+            + conta_da_Barbara.Saldo + ".") ;
+            Console.WriteLine ("A conta é do (a) " + conta_da_ana.Titular + 
+            ", a agência é " + conta_da_ana.Agencia + " e o nº é " + conta_da_ana.Numero + "e o saldo é de: "
+            + conta_da_ana.Saldo + ".") ;
+            Console.WriteLine ("A conta é do (a) " + conta_da_jessica.Titular + 
+            ", a agência é " + conta_da_jessica.Agencia + ", o nº é " + conta_da_jessica.Numero + "e o saldo é de: "
+            + conta_da_jessica.Saldo + ".") ;
+
+            bool sacar_conta1 = conta_da_Barbara.Sacar(100);
+            bool sacar_conta2 = conta_da_ana.Sacar(100);
+            bool sacar_conta3 = conta_da_jessica.Sacar(100);
 
             Console.WriteLine ("A conta é do (a) " + conta_da_Barbara.Titular + 
             ", a agência é " + conta_da_Barbara.Agencia + ", o nº é " + conta_da_Barbara.Numero + "e o saldo é de: "
