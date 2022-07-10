b1810dcb0b771db1fb9a62ef0b83814b631b81c9
analivia1104 <analivia110405@gmail.com>
2022-07-10 13:10:21 -0300

    Envio da função Depositar

--- ContaCorrente.cs
+++ ContaCorrente.cs
@@ -11,15 +11,17 @@ public class ContaCorrente
     public int Agencia;
     public int Numero;
     public double Saldo;
+    public double Depositar;
 
     // construtor: o mesmo nome da classe
-    public ContaCorrente (string Titular, int Agencia, int Numero, double Saldo) { 
+    public ContaCorrente (string Titular, int Agencia, int Numero, double Saldo, double Depositar) { 
         
         //this é a representação de um objeto: não é de alguém especifico.
         this.Titular = Titular;
         this.Agencia = Agencia;
         this.Numero = Numero;
-        this.Saldo = Saldo; 
+        this.Saldo = Saldo;
+        this.Depositar = Depositar; 
 
     } 
     public bool Sacar( double valorSaque )
@@ -30,7 +32,13 @@ public class ContaCorrente
         }
             return false;
     }
-
-    //Fazer o valor depositar
+    public bool Depositar( double valorDeposito )
+    { 
+        if(this.Depositar > 0 ){
+            this.Saldo = this.Saldo + valorDeposito ;
+            return true;
+        }
+            return false;
+    }
 
 }
